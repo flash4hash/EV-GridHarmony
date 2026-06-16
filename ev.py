ce0a252b2d5c1889cfda34a035078ecd0eae2aaa
flash4hash <248335544+flash4hash@users.noreply.github.com>
2026-06-16 18:27:46 +0530

    Added EV class with EV properties and energy calculation

--- ev.py
+++ ev.py
@@ -17,3 +17,15 @@ V2G Permission
 
 Fairness Credits
 '''
+
+class EV:
+    def __init__(self, ev_id, current_soc, target_soc, battery_cap, arrival, departure, user_pref, v2g):
+        self.ev_id = ev_id
+        self.current_soc = current_soc
+        self.target_soc = target_soc
+        self.battery_cap = battery_cap
+        self.arrival = arrival
+        self.departure = departure
+        self.user_pref = user_pref
+        self.v2g = v2g
+        self.energy_req = (target_soc - current_soc) * battery_cap
