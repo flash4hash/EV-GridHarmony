1ce2a1bc194f1047a67eacf53abc83841a34d301
flash4hash <248335544+flash4hash@users.noreply.github.com>
2026-06-16 23:12:36 +0530

    Completed core environment module

--- environment.py
+++ environment.py
@@ -4,3 +4,12 @@ The outside world:
     Solar generation curve
     Peak demand periods
 '''
+
+import config as cf
+
+class Environment:
+    def price(self, hour):
+        return cf.e_price[hour]
+
+    def solar_power(self, hour):
+        return cf.solar_gen[hour]
