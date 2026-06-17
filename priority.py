9adde7349d2417a47f2ea2d9f647b6f8c092901b
flash4hash <248335544+flash4hash@users.noreply.github.com>
2026-06-17 10:43:08 +0530

    Implemented EV priority scoring system

--- priority.py
+++ priority.py
@@ -1,1 +1,14 @@
 # Priority Score = User Tier + Urgency + Fairness Credits
+from config import user_tier
+
+def time_remaining(ev, current_hour):
+    if ev.departure < current_hour:
+        return 24 + ev.departure - current_hour
+    else:
+        return ev.departure - current_hour
+    
+def urgency(ev, current_hour):
+    return ev.energy_req/time_remaining(ev, current_hour)
+
+def priority_score(ev, current_hour):
+    return user_tier[ev.user_pref]["priority"] + urgency(ev, current_hour) + 0.5*ev.v2g
